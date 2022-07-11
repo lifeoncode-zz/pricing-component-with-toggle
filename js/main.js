d79d27d020214d1ae90031376167568e44932c8e
lifeoncode <ndu918@outlook.com>
2022-07-11 23:04:40 +0200

    desktop version complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const toggleContainer = document.querySelector('.toggle');
 const toggleBtn = toggleContainer.querySelector('.btn');
+const annualPrices = document.querySelectorAll('.annual-price');
+const monthlyPrices = document.querySelectorAll('.monthly-price');
+
 
 // toggle btn set to anual plan
 // switch to monthly plan when clicked for the first time
@@ -10,6 +13,7 @@ function switchToMonthly(e) {
     toggleBtn.removeEventListener('click', switchToMonthly);
     toggleBtn.addEventListener('click', switchToAnnually);
     animateBtn('monthly');
+    showPrices('monthly');
 }
 
 
@@ -17,6 +21,7 @@ function switchToAnnually(e) {
     toggleBtn.removeEventListener('click', switchToAnnually);
     toggleBtn.addEventListener('click', switchToMonthly);
     animateBtn('annually');
+    showPrices('annually');
 }
 
 
@@ -26,5 +31,25 @@ function animateBtn(plan) {
           
     } else if (plan === 'annually') {
         toggleBtn.classList.remove('move-right');        
+    }
+}
+
+
+function showPrices(plan) {
+    if (plan === 'monthly') {
+        annualPrices.forEach(price => {
+            price.classList.remove('show');
+        })
+        monthlyPrices.forEach(price => {
+            price.classList.add('show');
+        })
+
+    } else if (plan === 'annually') {
+        monthlyPrices.forEach(price => {
+            price.classList.remove('show');
+        })
+        annualPrices.forEach(price => {
+            price.classList.add('show');
+        })
     }
 }
